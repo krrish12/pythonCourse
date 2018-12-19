1de05e2f0fef932e6ad0accbc954267b2de2e512
krishna <krrish.12@gmail.com>
2018-12-19 13:41:12 +0530

    Create python3.py

---
+++ Day-3/python3.py
@@ -0,0 +1,16 @@
+obj=2*2
+print(obj)
+obj=10-4
+print(obj)#print int value
+obj=10-3.0
+print(obj)#print float value
+obj=2/2
+print(obj)
+obj=2%2
+print(obj)#print int value
+obj=2%2.0
+print(obj)#print float value
+obj=2.0%2
+print(obj)#print float value
+
+
