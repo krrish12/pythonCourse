f743d957a320f81897dad84a65e838f22a06bdb0
krishna <krrish.12@gmail.com>
2018-12-19 15:30:09 +0530

    Update python3.py

--- Day-3/python3.py
+++ Day-3/python3.py
@@ -1,23 +1,45 @@
 obj=10+4
-print(obj,type(obj))#int type
+print(obj)#int type
+#output: 14
+
 obj=10+3.0
-print(obj,type(obj))#float type
+print(obj)#float type
+#output:13.0
+
 obj=10-4
-print(obj,type(obj))#int type
+print(obj)#int type
+#output: 6
+
 obj=10-3.0
-print(obj,type(obj))#float type
+print(obj)#float type
+#output:7.0
+
 obj=2*2
-print(obj,type(obj))#int type
+print(obj)#int type
+#output: 4
+
 obj=4.0*2
-print(obj,type(obj))#float type
-obj=2/2#print int value
-print(obj,type(obj))#float type
+print(obj)#float type
+#output: 8.0
+
+obj=2/2
+print(obj)#float type
+#output: 1.0
+
 obj=2%2
-print(obj,type(obj))#int type
+print(obj)#int type
+#output:0
+
 obj=2%2.0
-print(obj,type(obj))#float type
+print(obj)#float type
+#output: 0.0
+
 obj=2.0%2
-print(obj,type(obj))#float type
+print(obj)#float type
+#output: 0.0
+
 obj=10+4j
-print(obj,type(obj))#complex type
+print(obj)#complex type
+#output:(10+4j)
+
 
