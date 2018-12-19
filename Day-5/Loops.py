e8335bb37d27d61a4c85f207b61f6402dec7c7cf
krishna <krrish.12@gmail.com>
2018-12-19 17:56:13 +0530

    Create Loops.py

---
+++ Day-5/Loops.py
@@ -0,0 +1,49 @@
+count = 0
+while count < 2:
+   print(count, " is  less than 2")
+   count = count + 1
+else:
+   print(count, " is not less than 2")
+
+#output:
+   # o is less than 2
+   # 1 is less than 2
+   # 2 is not less than 2
+
+for letter in 'Python':     # First Example
+   print('Current Letter: %c' % letter)
+else:
+    print("The End")
+
+#output:
+   #Current Letter: P
+   #Current Letter: y
+   #Current Letter: t
+   #Current Letter: h
+   #Current Letter: o
+   #Current Letter: n
+   #The End
+
+for num in range(10,15):
+    print('%d' % num)
+else:
+    print('%d' % (num+1))
+
+#output:
+    #10
+    #11
+    #12
+    #13
+    #14
+    #15
+for num in range(10,15):
+    print('%f' % num)
+
+#output:
+    #10.000000
+    #11.000000
+    #12.000000
+    #13.000000
+    #14.000000
+            
+
