2b3bf71e7f62d0417ea0052e2e7b79d55d177d0d
krishna <krrish.12@gmail.com>
2018-12-19 13:29:18 +0530

    Update python1.py

--- Day-1/python1.py
+++ Day-1/python1.py
@@ -7,4 +7,21 @@ obj,obj1 = 2,3
 print(obj,",",obj1)
 obj=obj1=obj2=10
 print(obj,",",obj1,",",obj2)
+#Arrays
+obj=["Hello",1254,2.0]
+print(obj)
+print(obj[2])
+print(obj[2:])
+#tuple
+obj=("Hello",1254,2.0)
+print(obj)
+print(obj[0])
+print(obj * 2)
+#Dictionary
+obj={}
+obj = {'name':'Micky'}
+print(obj)
+print(obj.keys())
+print(obj.values())
+print(obj['name'])
 
