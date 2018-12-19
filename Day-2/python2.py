ef273da56cdfa4e1100e356ffd68a9972dc3e183
krishna <krrish.12@gmail.com>
2018-12-19 13:30:45 +0530

    Create python2.py

---
+++ Day-2/python2.py
@@ -0,0 +1,15 @@
+# How to get Data type of the object
+obj=1
+print(type(obj))
+obj=2.0
+print(type(obj))
+obj="Hello"
+print(type(obj))
+obj=2+9j
+print(type(obj))
+obj=2+9J
+print(type(obj))
+obj=2+9.0
+print(type(obj))
+obj=9.
+print(type(obj))
