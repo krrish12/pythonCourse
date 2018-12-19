f391b6680cda563d4771724446c71f0e2970d047
krishna <krrish.12@gmail.com>
2018-12-19 17:08:24 +0530

    Create variableType.py

---
+++ Day-2/variableType.py
@@ -0,0 +1,28 @@
+# How to get Data type of the object
+obj=1
+print(type(obj))
+#output : <class 'int'>
+
+obj=2.0
+print(type(obj))
+#output :<class 'float'>
+
+obj="Hello"
+print(type(obj))
+#output :<class 'str'>
+
+obj=2+9j
+print(type(obj))
+#output :<class 'complex'>
+
+obj=2+9J
+print(type(obj))
+#output :<class 'complex'>
+
+obj=2+9.0
+print(type(obj))
+#output :<class 'float'>
+
+obj=9.
+print(type(obj))
+#output :<class 'float'>
