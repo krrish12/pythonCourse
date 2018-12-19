ea9b2813e9319d170188cf793e6c40ba8c454a0f
krishna <krrish.12@gmail.com>
2018-12-19 16:57:20 +0530

    Update operators.py

--- Day-3/operators.py
+++ Day-3/operators.py
@@ -42,4 +42,89 @@ obj=10+4j
 print(obj)#complex type
 #output:(10+4j)
 
+obj = 2**3
+print(obj)#int type
+#output: 8
+
+obj = 3//2
+print(obj)#int type
+#output: 1
+
+obj = 3/2.0
+print(obj)#float type
+#output: 1.5
+
+#Comparison Operators
+
+obj,obj1 = 2,3
+
+print(obj == obj1)#boolean type
+#output: False
+
+print(obj is obj1)
+#output: False
+
+print(obj != obj1)
+#output: True
+
+print(not(obj == obj1))
+#output: True
+
+print(not(obj != obj1))
+#output: False
+
+print(obj > obj1)
+#output: False
+
+print(obj < obj1)
+#output: False
+
+print(obj >= obj1)
+#output: True
+
+print(obj <= obj1)
+#output: False
+
+#Logical Operators
+
+print(obj and obj1)
+#output: 3
+
+print(obj or obj1)
+#output: 2
+
+#Bitwise Operators
+print(obj & obj1)
+#output: 2
+
+print(obj | obj1)
+#output: 3
+
+print(~obj)
+#output: -3
+
+print(obj ^ obj1)
+#output: 1
+
+print(obj>> 2)
+#output: 0
+
+print(obj << 5)
+#output: 64
+
+#Identity Operators
+
+print(obj is not obj1)
+#output: True
+
+print(obj is obj1)
+#output: False
+
+#Membership Operators
+a,b = 2,[1,2,3]
+print(a in b)
+#output: True
+
+print(a not in b)
+#output: False
 
