6149caf20c8542bf08a621d625b6614631bdeba1
krishna <krrish.12@gmail.com>
2018-12-20 15:38:16 +0530

    Update Lists.py

--- Day-8/Lists.py
+++ Day-8/Lists.py
@@ -1,51 +1,51 @@
-a,b=[10,20],[30,40]
+listObj,listObj1=[10,20],[30,40]
 
-print("Expression value is:",len(a))
+print("Expression value is:",len(listObj))
 #output: Expression value is: 2
 
-print("Expression value is:",max(a))
+print("Expression value is:",max(listObj))
 #output: Expression value is: 20
 
-print("Expression value is:",min(a))
+print("Expression value is:",min(listObj))
 #output: Expression value is: 10
 
 obj=(10,20,30)
 print("Expression value is:",list(obj))
 #output: Expression value is: [10, 20, 30]
 
-a.append(30)
-print("Expression value is:",a)
+listObj.append(30)
+print("Expression value is:",listObj)
 #output: Expression value is: [10, 20, 30]
 
-print("Expression value is:",a.count('30'))
+print("Expression value is:",listObj.count('30'))
 #output: Expression value is: 0
 
-print("Expression value is:",a.count(30))
+print("Expression value is:",listObj.count(30))
 #output: Expression value is: 1
 
-a.extend(b)
-print("Extended List : ", a)
+listObj.extend(listObj1)
+print("Extended List : ", listObj)
 #output: Extended List :  [10, 20, 30, 30, 40]
 
-print("Expression value is:",a.index(30))
+print("Expression value is:",listObj.index(30))
 #output: Expression value is: 2
 
-a.insert(3,35)
-print("Expression value is:",a)
+listObj.insert(3,35)
+print("Expression value is:",listObj)
 #output: Expression value is: [10, 20, 30, 35, 30, 40]
 
-a.pop()
-print("Expression value is:",a)
+listObj.pop()
+print("Expression value is:",listObj)
 #output: Expression value is: [10, 20, 30, 35, 30]
 
-a.remove(30)
-print("Expression value is:",a)
+listObj.remove(30)
+print("Expression value is:",listObj)
 #output: Expression value is: [10, 20, 35, 30]
 
-a.reverse()
-print("Expression value is:",a)
+listObj.reverse()
+print("Expression value is:",listObj)
 #output: Expression value is: [30, 35, 20, 10]
 
-a.sort(reverse=True)
-print("Expression value is:",a)
+listObj.sort(reverse=True)
+print("Expression value is:",listObj)
 #output: Expression value is: [35, 30, 20, 10]
