7ed224efec62a4efdbdb4df4226b6126fcd07edb
krishna <krrish.12@gmail.com>
2018-12-20 15:33:24 +0530

    Update Lists.py

--- Day-8/Lists.py
+++ Day-8/Lists.py
@@ -26,3 +26,26 @@ print("Expression value is:",a.count(30))
 a.extend(b)
 print("Extended List : ", a)
 #output: Extended List :  [10, 20, 30, 30, 40]
+
+print("Expression value is:",a.index(30))
+#output: Expression value is: 2
+
+a.insert(3,35)
+print("Expression value is:",a)
+#output: Expression value is: [10, 20, 30, 35, 30, 40]
+
+a.pop()
+print("Expression value is:",a)
+#output: Expression value is: [10, 20, 30, 35, 30]
+
+a.remove(30)
+print("Expression value is:",a)
+#output: Expression value is: [10, 20, 35, 30]
+
+a.reverse()
+print("Expression value is:",a)
+#output: Expression value is: [30, 35, 20, 10]
+
+a.sort(reverse=True)
+print("Expression value is:",a)
+#output: Expression value is: [35, 30, 20, 10]
